51089529014f0b1db557683c3cd50520515a51ec
Gaidysheff <gaidysheff@mail.ru>
2023-03-15 19:52:18 +0500

    Initial

---
+++ webpack.config.js
@@ -0,0 +1,140 @@
+const path = require("path");
+
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+const isDev = process.env.NODE_ENV === "development";
+const isProd = !isDev;
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: "all",
+    },
+  };
+  if (isProd) {
+    config.minimizer = [new TerserPlugin(), new CssMinimizerPlugin()];
+  }
+  return config;
+};
+
+const filename = (ext) => (isDev ? `[name].${ext}` : `[name].[hash].${ext}`);
+
+const cssLoaders = (extra) => {
+  const loaders = [
+    {
+      loader: MiniCssExtractPlugin.loader,
+    },
+    "css-loader",
+  ];
+  if (extra) {
+    loaders.push(extra);
+  }
+  return loaders;
+};
+
+const babelOptions = (preset) => {
+  const opts = {
+    presets: ["@babel/preset-env"],
+    plugins: [],
+  };
+  if (preset) {
+    opts.presets.push(preset);
+  }
+  return opts;
+};
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: ["@babel/polyfill", "./src/index.tsx"],
+  },
+  output: {
+    filename: filename("js"),
+    path: path.resolve(process.cwd(), "dist"),
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json", ".ts", ".tsx", ".svg", ".png"],
+  },
+
+  devtool: isDev ? "source-map" : false,
+
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/images/favicon.ico"),
+          to: path.resolve(__dirname, "dist"),
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: filename("css"),
+    }),
+  ],
+
+  optimization: optimization(),
+
+  devServer: {
+    // static: "./dist",
+    port: 3000,
+    hot: isDev,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: cssLoaders(),
+      },
+
+      {
+        test: /\.s[ac]ss$/i,
+        use: cssLoaders("sass-loader"),
+      },
+
+      {
+        test: /\.(png|jp?g|svg|gif)$/i,
+        type: "asset/resource",
+        // use: ["file-loader"],
+      },
+
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: "asset/inline",
+      },
+
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: babelOptions(),
+        },
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: babelOptions("@babel/preset-typescript"),
+        },
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: babelOptions("@babel/preset-react"),
+        },
+      },
+    ],
+  },
+};
