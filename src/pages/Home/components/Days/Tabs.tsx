e10cb99ea1b2502d09f2ca430b28a3eb0cec7c39
Gaidysheff <gaidysheff@mail.ru>
2023-03-20 15:02:36 +0500

    Section with Tabs is ready

--- src/pages/Home/components/Days/Tabs.tsx
+++ src/pages/Home/components/Days/Tabs.tsx
@@ -2,10 +2,35 @@ import React from "react";
 
 import style from "./Days.module.scss";
 
-type Props = {};
+interface Props {}
+export interface Tabs {
+  value: string;
+}
 
 const Tabs = (props: Props) => {
-  return <div className={style.tabs}>Tabs</div>;
+  const tabs: Tabs[] = [
+    {
+      value: "for week",
+    },
+    {
+      value: "for month",
+    },
+    {
+      value: "for 10 days",
+    },
+  ];
+  return (
+    <div className={style.tabs}>
+      <div className={style.tabs__wrapper}>
+        {tabs.map((tab) => (
+          <div className={style.tabs__tab} key={tab.value}>
+            {tab.value}
+          </div>
+        ))}
+      </div>
+      <div className={style.tabs__cancel}>Cancel</div>
+    </div>
+  );
 };
 
 export default Tabs;
