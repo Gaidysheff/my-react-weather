7cec6ce187139a6cbf9a71a0dc147d135130ef84
Gaidysheff <gaidysheff@mail.ru>
2023-03-21 23:20:33 +0500

    Slice for the current weather is done

---
+++ src/store/store.ts
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({});
+
+export const store = configureStore({
+  reducer: rootReducer,
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = typeof store;
+export type AppDispatch = AppStore["dispatch"];
