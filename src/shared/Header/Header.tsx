473b053d64c40c4061999e3212fb184c7a1a46a8
Gaidysheff <gaidysheff@mail.ru>
2023-03-21 16:15:11 +0500

    ThemeChange is ready

--- src/shared/Header/Header.tsx
+++ src/shared/Header/Header.tsx
@@ -1,14 +1,14 @@
-import React from "react";
 import Select from "react-select";
-import GlobalSvgSelector from "../../assets/icons/global/GlobalSvgSelector";
-
 import { StylesConfig } from "react-select";
-
+import GlobalSvgSelector from "../../assets/icons/global/GlobalSvgSelector";
+import { Theme } from "../../context/ThemeContext";
 import style from "./Header.module.scss";
+import { useTheme } from "../../hooks/useTheme";
 
 type Props = {};
 
 const Header = (props: Props) => {
+  const theme = useTheme();
   const options = [
     { value: "city-1", label: "Ekaterinburg" },
     { value: "city-2", label: "Moscow" },
@@ -18,7 +18,8 @@ const Header = (props: Props) => {
   const colourStyles: StylesConfig = {
     control: (styles) => ({
       ...styles,
-      backgroundColor: "rgba(71, 147, 255, 0.2)",
+      backgroundColor:
+        theme.theme === "dark" ? "#4f4f4f" : "rgba(71, 147, 255, 0.2)",
       borderRadius: "10px",
       width: "194px",
       height: "40px",
@@ -26,7 +27,15 @@ const Header = (props: Props) => {
       lineHeight: 1.5,
       zIndex: 100,
     }),
+    singleValue: (styles) => ({
+      ...styles,
+      color: theme.theme === "dark" ? "#fff" : "#000",
+    }),
   };
+
+  function changeTheme() {
+    theme.changeTheme(theme.theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT);
+  }
 
   return (
     <header className={style.header}>
@@ -37,7 +46,7 @@ const Header = (props: Props) => {
         <div className={style.header__title}>React weather</div>
       </div>
       <div className={style.header__wrapper}>
-        <div className={style.header__change_theme}>
+        <div className={style.header__change_theme} onClick={changeTheme}>
           <GlobalSvgSelector id="change-theme" />
         </div>
         <Select
